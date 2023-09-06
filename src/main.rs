f39f3a05fc2c49c1a9d3c00d56b4ebe18a7bdb44
0Date <144079182+0Date@users.noreply.github.com>
2023-09-06 14:54:54 -0600

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+#![allow(unused_imports)]
+
 mod hello_world;
 use hello_world::hello_world_main;
+
+mod variable_constant_shadowing;
+use variable_constant_shadowing::variable_constant_shadowing_main;
+
+mod primitive_data_types;
+use primitive_data_types::primitive_data_types_main;
+
+mod input_from_console;
+use input_from_console::input_from_console_main;
+
+mod arithmatic_and_type_casting;
+use arithmatic_and_type_casting::arithmatic_and_type_casting_main;
+
+//mod conditions_and_control_flow;
+//use conditions_and_control_flow::conditions_an_control_flow_main;
+
+//all functions are order in the way that this playlist is structured https://www.youtube.com/watch?v=T_KrYLW4jw8&list=PLzMcBGfZo4-nyLTlSRBvo0zjSnCnqjHYQ&index=1 
+
 fn main() {
-    hello_world_main();
+    /*
+    To use a specific file delete the "//" infront of the "{file name}_main"
+    Then hit 'Ctrl + s'
+    Then hit 'Ctrl + F5'
+    */
+
+    //hello_world_main();
+    //variable_constant_shadowing_main();
+    //primitive_data_types_main();
+    //input_from_console_main();
+    //arithmatic_and_type_casting_main();
 }
